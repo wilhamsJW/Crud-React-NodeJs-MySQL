bbe7f4f8766d25fff1cac14b1cd366ac0edb0ca5
wilhamsJW <wilhamsjunior@gmail.com>
2021-11-03 12:53:34 -0300

    creating function inputs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,54 @@
-import './App.css';
+import React, { useState } from "react"
+import './App.css'
 
 function App() {
+
+  const [values, setValues] = useState();
+  // console.log(setValues);
+
+  // [value.target.name]: value.target.value, -> está passando os valores para 0 "values"
+  // do useState() acima
+  const handleChangeValues = (value) => {
+    setValues(prevValue => ({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }))
+  }
+
+  const handleClickButton = () => {
+    console.log('handleClickButton', values);
+  }
+
   return (
-    <div className="App">
-      
+    <div className="app--container">
+      <div className="register--container">
+        <h1 className="register--title">Gerenciamento de Usuários</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          className="register--input"
+          onChange={handleChangeValues}
+        />
+        <input
+          type="text"
+          name="cost"
+          placeholder="Preço"
+          className="register--input"
+          onChange={handleChangeValues}
+        />
+        <input
+          type="text"
+          name="category"
+          placeholder="categoria"
+          className="register--input"
+          onChange={handleChangeValues}
+        />
+        <button className="register--button" onClick={() => handleClickButton()}>Cadastrar</button>
+      </div>
     </div>
   );
+
 }
 
 export default App;
